2abc82a813b50de23162f5b185eb4a879304474e
Ed Bosher <edbosher@gmail.com>
2013-10-29 14:19:50 +0800

    bumping and adding cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             'scout'
 maintainer        "Scout Support"
 maintainer_email  "supports@scoutapp.com"
 license           "Apache 2.0"
 description       "Installs and configures monitoring agent for scoutapp.com"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.4.1"
+version           "0.4.2"
 
 %w[ubuntu debian redhat centos fedora scientific amazon].each do |os|
   supports os
